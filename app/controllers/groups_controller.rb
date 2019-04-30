233bfdb04a6a549f345683ad1fee410306d355f6
yuzesu <toshikiohashi@ToshikinoMacBook-Pro.local>
2019-04-30 18:05:03 +0900

    side_bar編集

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
+  def index
+  end
+  
   def new
     @group = Group.new
     @group.users << current_user
@@ -16,9 +19,10 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name,{:user_ids => [] })
+    params.require(:group).permit(:name, user_ids: [] )
   end
 
   def set_group
     @group = Group.find(params[:id])
+  end
 end
