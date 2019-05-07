d73fb62ba0c667dcdd114b4eb84660c7033ed34d
yuzesu <toshikiohashi@ToshikinoMacBook-Pro.local>
2019-05-07 18:21:45 +0900

    root変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root'messages#index'
+  root'groups#index'
   resources :user, only: [:edit, :update]
-<<<<<<< Updated upstream
-=======
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
->>>>>>> Stashed changes
 end
