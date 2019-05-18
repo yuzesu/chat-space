29b8e3d08e87c499aeae07adf9ddb8969db594f6
yuzesu <toshikiohashi@ToshikinoMacBook-Pro.local>
2019-05-18 16:20:00 +0900

    エラー修正
    エラー修正完了
    グループ機能完成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  # before_action :set_group, only: [:edit, :update]
+  
   def index
   end
 
@@ -27,7 +29,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [] )
+    params.require(:group).permit(:name, { :user_ids => [] } )
   end
 
   def set_group
